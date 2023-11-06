8847462aa294d41eb23cf249b0fc674b7d347f5d
Chucky <carevisara@gmail.com>
2023-11-06 13:59:57 +0100

    removed dead code

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import (
-    Flask, 
-    render_template, 
-    send_from_directory
-)
+from flask import Flask, render_template
 import datetime
-import os
-
-from modules.constants import Constants
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return f"{Constants.project_name}: I'm alive. Move on..."
     context = {
         'date': datetime.datetime.utcnow().date()
     }
@@ -24,14 +16,6 @@ def about():
         'date': datetime.datetime.utcnow().date()
     }
     return render_template('about.html', context=context)
-
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(
-#         os.path.join(app.root_path, 'static'),
-#         'favicon.ico', 
-#         mimetype='image/vnd.microsoft.icon'
-#     )
 
 if __name__ == "__main__":
     #app.run(debug=True)
