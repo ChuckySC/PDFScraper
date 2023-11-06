a2505ad275e59510d3f0bd6f47d396872f4e846e
Chucky <carevisara@gmail.com>
2023-11-06 10:33:46 +0100

    added bootstrap, navigation, templates structure with base templates

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
-from flask import Flask, render_template
+from flask import (
+    Flask, 
+    render_template, 
+    send_from_directory
+)
 import datetime
+import os
 
 from modules.constants import Constants
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application
+def index():
     # return f"{Constants.project_name}: I'm alive. Move on..."
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow().date())
+    context = {
+        'date': datetime.datetime.utcnow().date()
+    }
+    return render_template('index.html', context=context)
+
+@app.route('/about')
+def about():
+    context = {
+        'date': datetime.datetime.utcnow().date()
+    }
+    return render_template('about.html', context=context)
+
+
+# TODO resolve GET /static/favicon.ico HTTP/1.1" 404
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico', 
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 if __name__ == "__main__":
     #app.run(debug=True)
